b0623bc817a74c03af4833044cf505b6330a7ffe
anastasiia.yukhymchuk <anastasiia.yukhymchuk@globalved.tech>
2025-02-10 21:02:16 +0200

    enable swagger

--- Program.cs
+++ Program.cs
@@ -9,12 +9,15 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+app.UseSwagger();
+app.UseSwaggerUI();
+
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
 
 app.UseHttpsRedirection();
 
